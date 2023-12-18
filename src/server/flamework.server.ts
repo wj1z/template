eb6cfd844e1fea339ad11cd852a72b8b6273f184
wj1z <143556225+wj1z@users.noreply.github.com>
2023-12-18 21:32:17 +0200

    Updated example

--- src/server/flamework.server.ts
+++ src/server/flamework.server.ts
@@ -1,4 +1,8 @@
 import { Flamework } from "@flamework/core";
+import { store } from "./store";
+
+store.set_shared_number(1);
+store.set_shared_string("Server!");
 
 Flamework.addPaths("src/server/services", "src/server/components", "src/shared/components");
 Flamework.ignite();
